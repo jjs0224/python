db5721466c1635bf16ff8c0e1c3bb595caa13a4c
jjs0224 <100536949+jjs0224@users.noreply.github.com>
2025-10-02 17:05:07 +0900

    Delete react/day6_lec directory

--- react/day6_lec/generatorEx3.py
+++
@@ -1,9 +0,0 @@
-def evenNum_generator(numbers):
-    for num in numbers:
-        if num % 2 == 0:
-            yield num
-
-numbers = list(map(int, input('정수들을 입력하세요: ').split()))
-
-for number in evenNum_generator(numbers):
-    print(number, end=" ")
