cd6b492e905087d655c3d01b9d508f101e7e9d0e
jjs0224 <100536949+jjs0224@users.noreply.github.com>
2025-10-02 17:08:49 +0900

    Delete react directory

--- react/day5_lec/fileEx1.py
+++
@@ -1,9 +0,0 @@
-fobj = open('hello.txt', 'w') #hello.txt 라는 파일을 만들어서 w = write mode 로 지정해줌
-fobj.write('hello python file~~~~~') #write() 를통해서 그안에 넣어줄내용을 적어줌 
-fobj.close() #파일 닫아주기 !필수!
-
-fobj2 = open('hello.txt', 'r') #hello.txt 파일을 열라고 정해주고 r = read only 로 지정해줌
-readStr = fobj2.read() #readStr 변수안에 파일을읽고 그내용을 그대로 넣어줌
-print(readStr) #hello python file~~~~~
-print(type(readStr)) #<class 'str'>
-fobj2.close() #파일닫아주기 !필수!
--- react/day5_lec/fileEx5.py
+++
@@ -1,4 +0,0 @@
-with open('Both.txt','r', encoding='utf-8') as file: #이방식사용하면 file.close() 해줄필요없음
-    rstr = file.read()
-    
-print(rstr)
